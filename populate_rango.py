a39bf81ee0e57a59e47bbd3bb5eabf81a2bb38e8
Reza <mrezzamoradi@gmail.com>
2017-02-01 00:05:47 +0330

    Models, templates and views

--- populate_rango.py
+++ populate_rango.py
@@ -10,20 +10,20 @@ from rango.models import Category, Page
 def populate():
 
     python_pages = [
-        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/"},
-        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/"},
-        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/"},
+        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/", "views": 24},
+        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/", "views": 14},
+        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": 35},
     ]
 
     django_pages = [
-        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title": "Django Rocks", "url": "http://www.djangorocks.com/"},
-        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"}
+        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 56},
+        {"title": "Django Rocks", "url": "http://www.djangorocks.com/", "views": 11},
+        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/", "views": 2}
     ]
 
     other_pages = [
-        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
-        {"title": "Flask", "url": "http://flask.pocoo.org"}
+        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 0},
+        {"title": "Flask", "url": "http://flask.pocoo.org", "views": 19}
     ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
@@ -42,7 +42,7 @@ def populate():
         # Updated the population script to pass through the specific values for views and likes
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rango.models import Category, Page
 
 # Create your views here.
 
 
 def index(request):
-    context_dic = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dic = {'categories': category_list, 'pages': page_list}
     return render(request, 'rango/index.html', context=context_dic)
 
 
 def about(request):
     context_dic = {'name': 'Reza Moradi'}
     return render(request, 'rango/about.html', context=context_dic)
+
+
+def show_category(request, category_name_slug):
+
+    context_dic = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dic['pages'] = pages
+        context_dic['category'] = category
+
+    except Category.DoesNotExist:
+        context_dic['pages'] = None
+        context_dic['category'] = None
+
+    return render(request, 'rango/category.html', context=context_dic)
+
